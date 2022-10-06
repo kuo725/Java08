309b64c651ab90ceb4a1150a524905930d0b2988
kuo725 <113486769+kuo725@users.noreply.github.com>
2022-10-06 22:21:39 +0800

    Create Class05.java

---
+++ Class05.java
@@ -0,0 +1,34 @@
+class CBox{
+	int length;
+	int width;
+	int height;
+	void volume() {
+		System.out.println("volume=" + length*width*height);
+	}
+	void surfaceArea() {
+		System.out.println("surfaceArea=" + (length*width + width*height + height*length)*2);
+	}
+	void showData() {
+		System.out.println("length=" + length);
+		System.out.println("width=" + width);
+		System.out.println("height=" + height);
+	}
+	void showAll() {
+		showData();
+		surfaceArea();
+		volume();
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CBox box;
+		box = new CBox();
+		box.length = 1;
+		box.width = 1;
+		box.height = 1;
+		box.showAll();
+	}
+
+}
